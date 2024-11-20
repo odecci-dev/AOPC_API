4ffaf2077348bf792367b3ea4c40fa1da009ee2a
odecci-dev <dev@odecci.com>
2024-11-20 21:35:05 +0800

    updated master

---
+++ API/ViewModel/CorporateUserCountVM.cs
@@ -0,0 +1,14 @@
+﻿namespace API.ViewModel
+{
+    public class CorporateUserCountVM
+    {
+        public string CorporateName { get; set; }
+        public string Registered {get; set; }
+        public string Unregistered { get; set; }
+        public string RegisteredVIP { get; set; }
+        public string TotalVIP { get; set; }
+        public string RemainingVip { get; set; }
+        public string UserCount { get; set; }
+        public string TotalUser { get; set; }
+    }
+}
---
+++ API/ViewModel/UserCountListing.cs
@@ -0,0 +1,13 @@
+﻿namespace API.ViewModel
+{
+    public class UserCountListing
+    {
+        public int registered { get; set; }
+        public int unregistered { get; set; }
+        public int isVIP { get; set; }
+        public int totalVIP { get; set; }
+        public int remainingVIP { get; set; }
+
+
+    }
+}
